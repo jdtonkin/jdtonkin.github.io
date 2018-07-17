8406b10993fa8194202136f64d5f161da3723f0d
Jonathan Tonkin <jdtonkin@gmail.com>
2018-07-17 16:53:56 -0700

    updated nw charge

--- assets/network/collab-nw.R
+++ assets/network/collab-nw.R
@@ -3,5 +3,5 @@ library(scholarnetwork)
 d <- extractNetwork(id="Mtn0TIwAAAAJ", n=500)
 str(d)
 
-plotNetwork(d$nodes, d$edges, file="network.html", width = 300, height = 300, charge = -15)
+plotNetwork(d$nodes, d$edges, file="network.html", width = 300, height = 300, charge = -4)
 ?plotNetwork
